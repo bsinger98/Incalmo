9aa9a0d20f1684022e6d1488d731c823e993a907
Brian Singer <brian@convolve.com>
2025-05-09 10:51:00 -0400

    Bsinger98/low level actions (#5)
    * Fix file names
    
    * Fix several low level actions
    
    * Fix list files
    
    * Fix more low level actions
    
    * resolve more conflicts

--- attacker/incalmo/actions/LowLevel/nikto_scan.py
+++ attacker/incalmo/actions/LowLevel/nikto_scan.py
@@ -1,41 +1,27 @@
-import re
+from ..low_level_action import LowLevelAction
+from models.attacker.agent import Agent
 
-from plugins.deception.app.actions.LowLevelAction import LowLevelAction
-
-from app.objects.c_agent import Agent
-from app.service.knowledge_svc import KnowledgeService
-from app.objects.c_operation import Operation
-from app.service.planning_svc import PlanningService
-
-from plugins.deception.app.models.events import Event, VulnerableServiceFound
+from models.events import Event, VulnerableServiceFound
 
 
 class NiktoScan(LowLevelAction):
-    ability_name = "deception-runbashcommand"
-
     def __init__(self, agent: Agent, host: str, port: int, service: str):
         self.host = host
         self.port = port
         self.service = service
 
         command = f"nikto -h {host} -p {port} -maxtime 10s -timeout 3"
-        facts = {"host.command.input": command}
-        self.command = command
-
-        super().__init__(agent, facts, self.ability_name)
+        super().__init__(agent, command)
 
     async def get_result(
         self,
-        operation: Operation,
-        planner: PlanningService,
-        knowledge_svc_handle: KnowledgeService,
-        raw_result: dict | None = None,
+        stdout: str | None,
+        stderr: str | None,
     ) -> list[Event]:
-        if raw_result is None:
+        if stdout is None:
             return []
-
-        output = raw_result["stdout"]
-        if "CVE-2017-5638" in output:
+        
+        if "CVE-2017-5638" in stdout:
             return [
                 VulnerableServiceFound(
                     port=self.port,
--- attacker/incalmo/models/events/ExfiltratedData.py
+++ attacker/incalmo/models/events/ExfiltratedData.py
@@ -1,13 +1,14 @@
 from incalmo.models.events import Event
 from incalmo.models.network import Host
 
-
 class ExfiltratedData(Event):
     def __init__(
         self,
         file: str,
+        hash: str
     ):
         self.file = file
+        self.hash = hash
 
     def __str__(self):
-        return f"ExfiltratedData: {self.file}"
+        return f"ExfiltratedData: {self.file} with hash {self.hash}"
