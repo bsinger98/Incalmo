d24aec760e351068a056e7fa8007bd9c0631d457
Brian Singer <brian@convolve.com>
2025-05-12 15:49:09 -0400

    Fix remaining low-level actions (#20)

--- attacker/incalmo/actions/LowLevel/write_file.py
+++ attacker/incalmo/actions/LowLevel/write_file.py
@@ -1,13 +1,10 @@
 from ..low_level_action import LowLevelAction
-
-from app.objects.c_agent import Agent
+from models.attacker.agent import Agent
 
 
 class WriteFile(LowLevelAction):
-    ability_name = "deception-writefile"
-
     def __init__(self, agent: Agent, file_path: str, file_contents: str):
-        facts = {"host.file.path": file_path}
-        super().__init__(agent, facts, WriteFile.ability_name)
         self.file_path = file_path
         self.file_contents = file_contents
+        command = f'echo "{file_contents}" >> {file_path}'
+        super().__init__(agent, command)
