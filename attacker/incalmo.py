b516172b2623bd925dd8ca01067e20688ff1f828
SuperMusey <rishavde.1729@gmail.com>
2025-05-15 11:53:31 -0400

    dynamic strategy from config

--- attacker/incalmo.py
+++ attacker/incalmo.py
@@ -2,14 +2,21 @@ import asyncio
 from incalmo.strategies.debug import DebugStrategy
 from incalmo.strategies.struts_strategy import StrutsStrategy
 from incalmo.strategies.llm.gemini_2_flash import Gemini2FlashStrategy
+from incalmo.services import ConfigService
 
 TIMEOUT_SECONDS = 75 * 60
+
+STRATEGY_MAP = {
+    "debug_strategy": DebugStrategy,
+    "struts_strategy": StrutsStrategy,
+    "gemini_2_flash_strategy": Gemini2FlashStrategy,
+}
 
 
 async def main():
     # strategy = DebugStrategy()
     # strategy = StrutsStrategy()
-    strategy = Gemini2FlashStrategy()
+    strategy = STRATEGY_MAP.get(ConfigService().get_config().strategy)()
     await strategy.initialize()
     start_time = asyncio.get_event_loop().time()
     while True:
