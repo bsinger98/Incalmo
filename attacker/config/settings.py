eec3b34bfe640a9a713532567fb62b07be399ffd
Brian Singer <brian@convolve.com>
2025-05-15 15:06:04 -0400

    Fix equifax vulnerability (#27)

--- attacker/config/settings.py
+++ attacker/config/settings.py
@@ -1,10 +1,11 @@
 # settings.py
-from pydantic import Field
 from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     # C2 server address
     c2_server: str
 
+
 # TODO fix this file
-settings = Settings(c2_server='http://0.0.0.0:8888')
+settings = Settings(c2_server="http://host.docker.internal:8888")
