27d636e7e703a25b6d203e2aa922e3bac9991469
Brian Singer <brian@convolve.com>
2025-05-26 19:30:46 -0400

    Cleanup file paths

--- test_api.py
+++ test_api.py
@@ -1,8 +1,10 @@
-from api.server_api import C2ApiClient
-from models.command_result import CommandResult
+from incalmo.api.server_api import C2ApiClient
+from incalmo.models.command_result import CommandResult
 from incalmo.core.actions.low_level_action import LowLevelAction
 from incalmo.core.models.events import Event
-from incalmo.core.services.low_level_action_orchestrator import LowLevelActionOrchestrator
+from incalmo.core.services.low_level_action_orchestrator import (
+    LowLevelActionOrchestrator,
+)
 import asyncio
 
 
@@ -20,7 +22,9 @@ async def main():
     for agent in prior_agents:
         print(agent.paw)
     action = WhoamiAction(
-        agent=prior_agents[0], command="ls -l", payloads=["test_attack.txt"]
+        agent=prior_agents[0],
+        command="ls -l",
+        payloads=["dynamic_payloads/test_attack.txt"],
     )
     orchestrator = LowLevelActionOrchestrator()
     events = await orchestrator.run_action(action)
