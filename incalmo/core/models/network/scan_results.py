931583daee72d16c08c897eac8eb0ab6b8ac3e1e
Brian Singer <brian@convolve.com>
2025-06-04 14:52:53 -0400

    Fix hosts

--- incalmo/core/models/network/scan_results.py
+++ incalmo/core/models/network/scan_results.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 from typing import List
-from .host import Host
+from .open_port import OpenPort
+
+
+class ScanHost(BaseModel):
+    ip: str
+    open_ports: List[OpenPort]
 
 
 class ScanResults(BaseModel):
-    results: List[Host]
+    results: List[ScanHost]
