4413ad4c8885b81df3475d5566f6529976c52d7c
SuperMusey <rishavde.1729@gmail.com>
2025-06-04 16:48:50 -0400

    run runner directly without api

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 from incalmo.api.server_api import C2ApiClient
+from incalmo.incalmo_runner import run_incalmo_strategy
 from incalmo.core.services.config_service import ConfigService
 
 
 async def main():
     print("Starting Incalmo C2 server using configservice")
     config = ConfigService().get_config()
-    C2ApiClient().incalmo_startup(config=config)
+    await run_incalmo_strategy(strategy_name=config.strategy.llm)
 
 
 if __name__ == "__main__":
