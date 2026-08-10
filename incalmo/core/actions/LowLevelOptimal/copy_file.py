df128e6fe8bb13620263a9e219055e8ccdde7758
peacock4o <mpmorris@andrew.cmu.edu>
2026-08-10 23:05:52 +0000

    Optimal Attacker strategy and actions

---
+++ incalmo/core/actions/LowLevelOptimal/copy_file.py
@@ -0,0 +1,18 @@
+from ..low_level_action import LowLevelAction
+
+from incalmo.models.agent import Agent
+
+
+class CopyFile(LowLevelAction):
+    def __init__(
+        self,
+        agent: Agent,
+        source_path: str,
+        destination_path: str,
+        high_level_action_id: str,
+    ):
+        self.source_path = source_path
+        self.destination_path = destination_path
+
+        command = f"cp {source_path} {destination_path}"
+        super().__init__(agent, command)
